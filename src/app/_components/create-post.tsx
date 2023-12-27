1ec98c610f188fa118b49b95fe3b1d2da97d5430
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-12-27 06:55:42 +0300

    Format Files

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 import { api } from "~/trpc/react";
 
--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,5 +1,7 @@
 import "server-only";
 
+import { cache } from "react";
+import { cookies } from "next/headers";
 import {
   createTRPCProxyClient,
   loggerLink,
@@ -8,8 +10,6 @@ import {
 import { callProcedure } from "@trpc/server";
 import { observable } from "@trpc/server/observable";
 import { type TRPCErrorResponse } from "@trpc/server/rpc";
-import { cookies } from "next/headers";
-import { cache } from "react";
 
 import { appRouter, type AppRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
