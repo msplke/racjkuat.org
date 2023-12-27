b468458db7041d3ca78e2a8c51ac90faf18daa63
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-12-27 06:54:02 +0300

    Feat: Configure Formatting with Prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,23 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindConfig: "./tailwind.config.ts",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
 };
 
 export default config;
