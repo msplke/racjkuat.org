0736f2b4e9004bc3cf9c20d11201a30e7fb935a7
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-03-02 07:09:52 +0300

    Rfctr: Drizzle Config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 import { env } from "~/env";
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema",
   driver: "pg",
   dbCredentials: {
     connectionString: env.DATABASE_URL,
   },
   tablesFilter: ["racjkuat_*"],
-} satisfies Config;
+});
