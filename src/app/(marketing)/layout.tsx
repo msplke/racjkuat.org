8e2107e490c2ce0716229b8da51503fd06fbd0a1
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-03-02 05:46:05 +0300

    Fix: Dashboard & Settings Pages

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
+
 import { MainNav, SiteFooter } from "~/components";
+import { buttonVariants } from "~/components/ui/button";
 import { marketingConfig } from "~/config/marketing";
+import { cn } from "~/lib/utils";
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -12,14 +16,14 @@ export default function MarketingLayout({ children }: MarketingLayoutProps) {
         <div className="flex h-20 items-center justify-between py-6">
           <MainNav items={marketingConfig.mainNav} />
 
-          {/* <nav>
+          <nav>
             <Link
               href="/login"
               className={cn(buttonVariants({ variant: "secondary" }), "px-4")}
             >
               Login
             </Link>
-          </nav> */}
+          </nav>
         </div>
       </header>
 
