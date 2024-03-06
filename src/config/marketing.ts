5c03c463f1a7fab4aa4dbe805fca0306d8192cb6
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-03-06 05:51:17 +0300

    Update marketing.ts

--- src/config/marketing.ts
+++ src/config/marketing.ts
@@ -17,28 +17,34 @@ export const marketingConfig: MarketingConfig = {
 
 export const benefits: BenefitItem[] = [
   {
-    title: "Social Impact ",
-    description: "Rotaract clubs bring together people ages 18 and older to exchange ideas with leaders in the community,",
+    title: "Networking",
+    description:
+      "Rotaract clubs bring together people ages 18 and older to exchange ideas with leaders in the community.",
   },
   {
-    title: "Development of leadership and professional skills,",
-    description: "Rotaract provide an opportunity for young people to practice their leadership and professional skills.It provides opportunities for members to develop leadership skills through organizing projects, managing teams, and holding leadership positions within the club.",
+    title: "Development of leadership and professional skills",
+    description:
+      "Rotaract provides an opportunity for young people to develop their professional and leadership skills through organizing projects, managing teams, and holding leadership positions within the club.",
   },
   {
     title: "Personal Growth",
-    description: "Rotaract encourages personal growth by offering opportunities for members to step out of their comfort zones, take on new challenges, and develop interpersonal and communication skills.",
+    description:
+      "Rotaract encourages personal growth by offering opportunities for members to step out of their comfort zones, take on new challenges, and develop interpersonal and communication skills.",
   },
   {
     title: "Community Service",
-    description: "Rotaract clubs engage in meaningful community service projects, allowing members to make a positive impact on their local communities and contribute to important causes..",
+    description:
+      "Rotaract clubs engage in meaningful community service projects, allowing members to have a positive impact on their local communities and contribute to important causes.",
   },
   {
     title: "International Connections",
-    description: "Rotaract is a global organization, providing opportunities for members to connect with young adults from different countries and cultures, fostering cross-cultural understanding and collaboration.",
+    description:
+      "Rotaract is a global organization, providing opportunities for members to connect with young adults from different countries and cultures, fostering cross-cultural understanding and collaboration.",
   },
   {
     title: "Access to Resources",
-    description: " Being part of a larger international organization like Rotary International provides Rotaract clubs with access to resources, funding, and support for their projects and initiatives.",
+    description:
+      "Being part of a larger international organization like Rotary International provides Rotaract clubs with access to resources, funding, and support for their projects and initiatives.",
   },
 ];
 
