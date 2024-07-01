b2453cd7bcd1d8a44b00c166537e2b0c35c4e50c
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-07-01 10:35:59 +0300

    fix: Update `getBaseUrl` function to use production url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -79,7 +79,8 @@ export function formatDate(input: string | number): string {
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin; // Browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use Vercel url
+  if (process.env.VERCEL_URL)
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`; // SSR should use Vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // Dev SSR should use localhost
 }
 
