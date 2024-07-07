114f827ee41b8e601c7a836b4d4ac9ed29949c0a
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-07-07 12:14:15 +0300

    refactor: Update `NavBar`

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,12 +1,7 @@
-import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
 
-import { Icons } from "~/components/icons";
-import { MainNav } from "~/components/layout/main-nav";
+import { NavBar } from "~/components/layout/navbar";
 import { SiteFooter } from "~/components/layout/site-footer";
-import { buttonVariants } from "~/components/ui/button";
-import { marketingConfig } from "~/config/marketing";
-import { cn } from "~/lib/utils";
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -17,27 +12,7 @@ export default function MarketingLayout({ children }: MarketingLayoutProps) {
 
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="container z-40 bg-background">
-        <div className="flex h-20 items-center justify-between py-6">
-          <MainNav items={marketingConfig.mainNav} />
-
-          <nav>
-            <Link
-              href={userId ? "/dashboard" : "/login"}
-              className={cn(buttonVariants({ variant: "secondary" }), "px-4")}
-            >
-              {userId ? (
-                <span className="flex items-center">
-                  Dashboard
-                  <Icons.chevronRight className="ml-1 h-4 w-4" />
-                </span>
-              ) : (
-                <span>Login</span>
-              )}
-            </Link>
-          </nav>
-        </div>
-      </header>
+      <NavBar scroll={true} userId={userId} />
 
       <main className="flex-1">{children}</main>
 
---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Link from "next/link";
+
+import { Icons } from "~/components/icons";
+import { MaxWidthWrapper } from "~/components/max-width-wrapper";
+import { buttonVariants } from "~/components/ui/button";
+import { marketingConfig } from "~/config/marketing";
+import { useScroll } from "~/hooks/use-scroll";
+import { cn } from "~/lib/utils";
+import { MainNav } from "./main-nav";
+
+interface NavBarProps {
+  scroll?: boolean;
+  userId: string | null;
+}
+
+export function NavBar({ scroll = false, userId }: NavBarProps) {
+  const scrolled = useScroll(50);
+
+  return (
+    <header
+      className={`sticky top-0 z-40 flex w-full justify-center bg-background/60 backdrop-blur-xl transition-all ${
+        scroll ? (scrolled ? "border-b" : "bg-transparent") : "border-b"
+      }`}
+    >
+      <MaxWidthWrapper className="flex h-16 items-center justify-between py-4">
+        <MainNav items={marketingConfig.mainNav} />
+
+        <nav>
+          <Link
+            href={userId ? "/dashboard" : "/login"}
+            className={cn(buttonVariants({ variant: "secondary" }), "px-4")}
+          >
+            {userId ? (
+              <span className="flex items-center">
+                Dashboard
+                <Icons.chevronRight className="ml-1 h-4 w-4" />
+              </span>
+            ) : (
+              <span>Login</span>
+            )}
+          </Link>
+        </nav>
+      </MaxWidthWrapper>
+    </header>
+  );
+}
