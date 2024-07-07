652e316d6d1eb0049e61138654b452f411f6af76
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-07-07 12:15:14 +0300

    refactor: Update `SiteFooter`

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -5,38 +5,41 @@ import { ModeToggle } from "~/components/mode-toggle";
 import { buttonVariants } from "~/components/ui/button";
 import { footerItems } from "~/config/marketing";
 import { cn } from "~/lib/utils";
+import { MaxWidthWrapper } from "../max-width-wrapper";
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.logo />
-          <p className="text-center text-sm leading-loose md:text-left">
-            © {new Date().getFullYear()} Rotaract Club of JKUAT. All rights
-            reserved.
-          </p>
-        </div>
-
-        <div className="flex flex-col items-center gap-2 md:flex-row lg:gap-4">
-          <div className="ml-auto text-sm text-primary">
-            {footerItems.map((item, index) => (
-              <Link
-                key={index}
-                href={item.disabled ? "#" : item.href}
-                target="_blank"
-                referrerPolicy="no-referrer"
-                className={buttonVariants({ variant: "ghost" })}
-              >
-                <item.icon className="h-4 w-4" />
-                <span className="sr-only">{item.title}</span>
-              </Link>
-            ))}
+      <MaxWidthWrapper>
+        <div className="flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+          <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+            <Icons.logo />
+            <p className="text-center text-sm leading-loose md:text-left">
+              © {new Date().getFullYear()} Rotaract Club of JKUAT. All rights
+              reserved.
+            </p>
           </div>
 
-          <ModeToggle />
+          <div className="flex flex-col items-center gap-2 md:flex-row lg:gap-4">
+            <div className="ml-auto text-sm text-primary">
+              {footerItems.map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.disabled ? "#" : item.href}
+                  target="_blank"
+                  referrerPolicy="no-referrer"
+                  className={buttonVariants({ variant: "ghost" })}
+                >
+                  <item.icon className="h-4 w-4" />
+                  <span className="sr-only">{item.title}</span>
+                </Link>
+              ))}
+            </div>
+
+            <ModeToggle />
+          </div>
         </div>
-      </div>
+      </MaxWidthWrapper>
     </footer>
   );
 }
