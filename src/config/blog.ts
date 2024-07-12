210de2fe8536bc09aeb79ca3f89612a93d55a1cd
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-07-12 07:42:01 +0300

    chore: Update author information

--- src/config/blog.ts
+++ src/config/blog.ts
@@ -1,13 +1,21 @@
-export const BLOG_AUTHORS = {
-  mickasmt: {
-    name: "mickasmt",
-    image: "/images/avatars/shadcn.png",
-    twitter: "miickasmt",
+export const BLOG_AUTHORS: Record<
+  string,
+  {
+    name: string;
+    image: string;
+    link?: string;
+  }
+> = {
+  kibuchi: {
+    name: "Peter Kibuchi",
+    image:
+      "https://gravatar.com/avatar/6b24bb0221d58ff25a5279fe6d5aec339681fcf96d1650f6b54eb5675e377af1?d=retro",
+    link: "https://github.com/peterkibuchi",
   },
-  shadcn: {
-    name: "shadcn",
-    image: "/images/avatars/shadcn.png",
-    twitter: "shadcn",
+  wamweru: {
+    name: "Peninah Wamweru",
+    image:
+      "https://gravatar.com/avatar/25249cd395309ed59b3b22785f0e61f35926e31b0b8d67af68847d4bb5c4f6b7?d=retro",
   },
 };
 
