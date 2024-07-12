aa661c28787a1b09bf279ab37d74c49658da5773
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-07-12 07:39:45 +0300

    feat: Improve rendering of author information in the Authors component

--- src/components/content/authors.tsx
+++ src/components/content/authors.tsx
@@ -11,36 +11,42 @@ export function Authors({
   imageOnly?: boolean;
 }) {
   const authors = BLOG_AUTHORS;
+  const author = authors[username];
+
+  // Check if the author exists
+  if (!author) {
+    // Handle the case where the author is not found. This could be rendering nothing, a placeholder, or logging an error.
+    console.error(`Author not found: ${username}`);
+    return null; // Example: return null to render nothing
+  }
 
   return imageOnly ? (
     <Image
-      src={authors[username as keyof typeof authors].image}
-      alt={authors[username as keyof typeof authors].name}
+      src={author.image}
+      alt={author.name}
       width={32}
       height={32}
       className="size-8 rounded-full transition-all group-hover:brightness-90"
     />
   ) : (
     <Link
-      href={`https://twitter.com/${authors[username as keyof typeof authors].twitter}`}
+      href={author.link ?? "#"}
       className="group flex w-max items-center space-x-2.5"
       target="_blank"
       rel="noopener noreferrer"
     >
       <Image
-        src={authors[username as keyof typeof authors].image}
-        alt={authors[username as keyof typeof authors].name}
+        src={author.image}
+        alt={author.name}
         width={40}
         height={40}
         className="size-8 rounded-full transition-all group-hover:brightness-90 md:size-10"
       />
       <div className="flex flex-col -space-y-0.5">
         <p className="font-semibold text-foreground max-md:text-sm">
-          {authors[username as keyof typeof authors].name}
+          {author.name}
         </p>
-        <p className="text-sm text-muted-foreground">
-          @{authors[username as keyof typeof authors].twitter}
-        </p>
+        <p className="text-sm text-muted-foreground">@{username}</p>
       </div>
     </Link>
   );
