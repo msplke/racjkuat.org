ed8f1fdf59da3f0cb9785cff7be9a4402f9ac73e
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-11-17 12:48:41 +0300

    fix: update author images to use local paths instead of Gravatar

--- next.config.js
+++ next.config.js
@@ -14,11 +14,6 @@ const config = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "gravatar.com",
-        pathname: "/avatar/**",
-      },
-      {
-        protocol: "https",
         hostname: "utfs.io",
         pathname: "/a/cxjgggfg3h/**",
       },
--- src/config/blog.ts
+++ src/config/blog.ts
@@ -8,29 +8,24 @@ export const BLOG_AUTHORS: Record<
 > = {
   eliza: {
     name: "Elizabeth Kamau",
-    image:
-      "https://gravatar.com/avatar/833dc869548f1c4cf789a9fed733def2aae92697070cb31066b883ea6f91d4aa?d=retro",
+    image: "/avatars/eliza.png",
   },
   hafsa: {
     name: "Hafsa Hajir",
-    image:
-      "https://gravatar.com/avatar/0cfd23e789f550b0187219876cb8f64b779e5c5514a679e6e1722ec1365eaff3?d=retro",
+    image: "/avatars/hafsa.png",
   },
   kibuchi: {
     name: "Peter Kibuchi",
-    image:
-      "https://gravatar.com/avatar/6b24bb0221d58ff25a5279fe6d5aec339681fcf96d1650f6b54eb5675e377af1?d=retro",
+    image: "/avatars/kibuchi.png",
     link: "https://github.com/peterkibuchi",
   },
   wamweru: {
     name: "Peninah Wamweru",
-    image:
-      "https://gravatar.com/avatar/25249cd395309ed59b3b22785f0e61f35926e31b0b8d67af68847d4bb5c4f6b7?d=retro",
+    image: "/avatars/wamweru.png",
   },
   wanjiru: {
     name: "Wanjiru Wanjiku",
-    image:
-      "https://gravatar.com/avatar/10cda03772fa57eb1fc245078c3190a40500d7312cb3449a5c655ce858ac5766?d=retro",
+    image: "/avatars/wanjiru.png",
   },
 };
 
